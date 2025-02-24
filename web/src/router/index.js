8bfd905f5d15a5a8f5d46c03aa207d491a6a12e8
hejz <75412985@qq.com>
2025-02-24 15:15:21 +0800

    前后端页面数据对接

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,8 +1,7 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import MainLayout from '../components/MainLayout.vue';
 import Login from "@/components/Login";
 import MachineStatus from '@/components/MachineStatus.vue';
-import MachineSettings from '../components/MachineSettings.vue';
 
 // 动态导入组件，提升性能
 const OrderPage = () => import('../components/OrderPage.vue');
@@ -18,30 +17,31 @@ const PriceSettings = () => import('../components/PriceSettings.vue');
 const SetAccount = () => import('@/components/SetAccount.vue');
 
 const routes = [
-    {path: '/login', name: 'Login', component: Login},
+    { path: '/login', name: 'Login', component: Login },
     {
         path: '/',
         component: MainLayout,
         children: [
-            {path: '', name: 'OrderPage', component: OrderPage}, // 默认首页
-            {path: 'buttons', name: 'ButtonsPage', component: ButtonsPage},
-            {path: 'salesStatistics', name: 'SalesStatistics', component: SalesStatistics},
-            {path: 'cleaningOperation', name: 'CleaningOperation', component: CleaningOperation},
-            {path: 'automaticOperation', name: 'AutomaticOperation', component: AutomaticOperation},
-            {path: 'manualOperation', name: 'ManualOperation', component: ManualOperation},
-            {path: 'alarmSettings', name: 'AlarmSettings', component: AlarmSettings},
-            {path: 'portionSettings', name: 'PortionSettings', component: PortionSettings},
-            {path: 'orderingSettings', name: 'OrderingSettings', component: OrderingSettings},
-            {path: 'priceSettings', name: 'PriceSettings', component: PriceSettings},
-            {path: 'setAccount', name: 'SetAccount', component: SetAccount},
-            {path: 'machineStatus', name: 'MachineStatus', component: MachineStatus},
-            {path: 'machineSettings', name: 'MachineSettings', component: MachineSettings}
+            { path: '', name: 'OrderPage', component: OrderPage }, // 默认首页
+            { path: 'buttons', name: 'ButtonsPage', component: ButtonsPage },
+            { path: 'salesStatistics', name: 'SalesStatistics', component: SalesStatistics },
+            { path: 'cleaningOperation', name: 'CleaningOperation', component: CleaningOperation },
+            { path: 'automaticOperation', name: 'AutomaticOperation', component: AutomaticOperation },
+            { path: 'manualOperation', name: 'ManualOperation', component: ManualOperation },
+            { path: 'alarmSettings', name: 'AlarmSettings', component: AlarmSettings },
+            { path: 'portionSettings', name: 'PortionSettings', component: PortionSettings },
+            { path: 'orderingSettings', name: 'OrderingSettings', component: OrderingSettings },
+            { path: 'priceSettings', name: 'PriceSettings', component: PriceSettings },
+            { path: 'setAccount', name: 'SetAccount', component: SetAccount },
+            { path: 'machineStatus', name: 'MachineStatus', component: MachineStatus }
         ]
     }
 ];
 
 // 假设使用 localStorage 存储用户的登录状态
-return !!localStorage.getItem('userToken'); // 假设通过 token 判断是否登录
+const isAuthenticated = () => {
+    return !!localStorage.getItem('userToken'); // 假设通过 token 判断是否登录
+};
 
 const router = createRouter({
     history: createWebHistory(), // 使用 HTML5 History 模式，去掉路径中的 # 号
